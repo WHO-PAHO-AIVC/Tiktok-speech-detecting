854fc0713a17ca1a5d57c3abfd7c1e6757d208ae
davidyushen <davidyushen@qq.com>
2021-11-08 18:12:40 -0600

    2nd attempt

--- Speach detecting from video to txt.py
+++ Speach detecting from video to txt.py
@@ -6,8 +6,8 @@ from tkinter import filedialog as fd
 def transfer_mp4_to_wav(input_video):
     video = moviepy.editor.VideoFileClip(input_video)
     audio = video.audio
-    audio.write_audiofile("test1.wav")
-    print("Completed")
+    audio.write_audiofile("test.wav")
+    print("Transforming completed")
 
 
 def speech_detecting_from_wav(input_audio_file):
@@ -21,7 +21,9 @@ def speech_detecting_from_wav(input_audio_file):
 
     with open("test.txt", mode="w") as file:
         file.write("Recognized text: ")
-        file.write("/n", result)
+        file.write("/n")
+        file.write(result)
+    print('Detecting completed')
 
 
 def main():
