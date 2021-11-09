bccb5273e42499eacdf309f0e6ab4dd1ae6d8443
Sreenithi <sreenithi.bala4@gmail.com>
2021-11-08 21:55:57 -0600

    Update Speach detecting from video to txt.py

--- Speach detecting from video to txt.py
+++ Speach detecting from video to txt.py
@@ -1,21 +1,27 @@
 import speech_recognition as sr
 import moviepy.editor
 from tkinter import filedialog as fd
+import os
+import json
 
 
-def transfer_mp4_to_wav(input_video):
-    video = moviepy.editor.VideoFileClip(input_video)
+def transfer_mp4_to_wav(input_video, path_input_video, save_path_wav):
+    print(path_input_video)
+    video = moviepy.editor.VideoFileClip(path_input_video)
     audio = video.audio
-    audio.write_audiofile("test.wav")
+
+    wav_filename = input_video.split(".mp4")[0] + ".wav"
+    audio.write_audiofile(os.path.join(save_path_wav, wav_filename))
+
     audio.close()
     video.close()
     print("Transforming completed")
 
 
 
-def speech_detecting_from_wav(input_audio_file):
+def speech_detecting_from_wav(input_audio_file, path_input_audio_file, save_path_txt):
     r = sr.Recognizer()
-    audio_file = sr.AudioFile(input_audio_file)
+    audio_file = sr.AudioFile(path_input_audio_file)
 
     with audio_file as source:
         # r.adjust_for_ambient_noise(source)
@@ -24,19 +30,38 @@ def speech_detecting_from_wav(input_audio_file):
         result = r.recognize_google(audio)
         print(result)
 
-    with open("test.txt", mode="w") as file:
+    filename = input_audio_file.split(".wav")[0] + ".txt"
+    with open(os.path.join(save_path_txt, filename), mode="w") as file:
         file.write("Recognized text: ")
         # change /n to \n
         file.write("\n")
         file.write(result)
+
     print('Detecting completed')
 
 
-def main():
-    input_video = fd.askopenfilename()
-    transfer_mp4_to_wav(input_video)
-    input_audio_file = fd.askopenfilename()
-    speech_detecting_from_wav(input_audio_file)
+if __name__ == '__main__':
+    f = open("configs.json")
+    configs = json.load(f)
 
+    mp4_data_path = configs["mp4_data_path"]
+    save_path_txt = configs["save_path_txt"]
+    save_path_wav = configs["save_path_wav"]
 
-main()
+    if not os.path.exists(save_path_wav):
+        os.mkdir(save_path_wav)
+
+    if not os.path.exists(save_path_txt):
+        os.mkdir(save_path_txt)
+
+    for input_video in os.listdir(mp4_data_path):
+        if input_video.split(".mp4")[-1] == "":
+            transfer_mp4_to_wav(input_video, os.path.join(mp4_data_path, input_video), save_path_wav)
+
+    print("Wav files saved in: ", save_path_wav)
+
+    for input_audio_file in os.listdir(save_path_wav):
+        if input_audio_file.split(".wav")[-1] == "":
+            speech_detecting_from_wav(input_audio_file, os.path.join(save_path_wav, input_audio_file), save_path_txt)
+
+    print("Text files saved in: ", save_path_txt)
